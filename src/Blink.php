4a04ea033c6709197bab086929aa3d99c7d2ed25
freek <freek@spatie.be>
2017-03-14 23:34:15 +0100

    commit

---
+++ src/Blink.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace Spatie\Blink;
+
+use Countable;
+use ArrayAccess;
+
+class Blink implements ArrayAccess, Countable
+{
+    /** @var array */
+    protected $values = [];
+
+    /**
+     * Put a value in the store.
+     *
+     * @param string|array    $name
+     * @param string|int|null $value
+     *
+     * @return $this
+     */
+    public function put($name, $value = null)
+    {
+        $newValues = $name;
+
+        if (! is_array($name)) {
+            $newValues = [$name => $value];
+        }
+
+        $this->values = array_merge($this->values, $newValues);
+
+        return $this;
+    }
+
+    /**
+     * Get a value from the store.
+     *
+     * @param string $name
+     * @param $default
+     *
+     * @return null|string
+     */
+    public function get(string $name, $default = null)
+    {
+        return $this->has($name)
+            ? $this->values[$name]
+            : $default;
+    }
+
+    /*
+     * Determine if the store has a value for the given name.
+     */
+    public function has(string $name) : bool
+    {
+        return array_key_exists($name, $this->values);
+    }
+
+    /**
+     * Get all values from the store.
+     *
+     * @return array
+     */
+    public function all() : array
+    {
+        return $this->values;
+    }
+
+    /**
+     * Get all keys starting with a given string from the store.
+     *
+     * @param string $startingWith
+     *
+     * @return array
+     */
+    public function allStartingWith(string $startingWith = '') : array
+    {
+        if ($startingWith === '') {
+            return $this->values;
+        }
+
+        return $this->filterKeysStartingWith($this->values, $startingWith);
+    }
+
+    /**
+     * Forget a value from the store.
+     *
+     * @param string $key
+     *
+     * @return $this
+     */
+    public function forget(string $key)
+    {
+        unset($this->values[$key]);
+
+        return $this;
+    }
+
+    /**
+     * Flush all values from the store.
+     *
+     * @return $this
+     */
+    public function flush()
+    {
+        return $this->values = [];
+    }
+
+    /**
+     * Flush all values which keys start with a given string.
+     *
+     * @param string $startingWith
+     *
+     * @return $this
+     */
+    public function flushStartingWith(string $startingWith = '')
+    {
+        if ($startingWith !== '') {
+            $this->values = $this->filterKeysNotStartingWith($this->values, $startingWith);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get and forget a value from the store.
+     *
+     * @param string $name
+     *
+     * @return null|string
+     */
+    public function pull(string $name)
+    {
+        $value = $this->get($name);
+
+        $this->forget($name);
+
+        return $value;
+    }
+
+    /**
+     * Increment a value from the store.
+     *
+     * @param string $name
+     * @param int    $by
+     *
+     * @return int|null|string
+     */
+    public function increment(string $name, int $by = 1)
+    {
+        $currentValue = $this->get($name) ?? 0;
+
+        $newValue = $currentValue + $by;
+
+        $this->put($name, $newValue);
+
+        return $newValue;
+    }
+
+    /**
+     * Decrement a value from the store.
+     *
+     * @param string $name
+     * @param int    $by
+     *
+     * @return int|null|string
+     */
+    public function decrement(string $name, int $by = 1)
+    {
+        return $this->increment($name, $by * -1);
+    }
+
+    /**
+     * Whether a offset exists.
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * Offset to retrieve.
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * Offset to set.
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->put($offset, $value);
+    }
+
+    /**
+     * Offset to unset.
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        $this->forget($offset);
+    }
+
+    /**
+     * Count elements.
+     *
+     * @link http://php.net/manual/en/countable.count.php
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->all());
+    }
+
+    protected function filterKeysStartingWith(array $values, string $startsWith) : array
+    {
+        return array_filter($values, function ($key) use ($startsWith) {
+            return $this->startsWith($key, $startsWith);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+
+    protected function filterKeysNotStartingWith(array $values, string $startsWith) : array
+    {
+        return array_filter($values, function ($key) use ($startsWith) {
+            return ! $this->startsWith($key, $startsWith);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+
+    protected function startsWith(string $haystack, string $needle) : bool
+    {
+        return substr($haystack, 0, strlen($needle)) === $needle;
+    }
+}
---
+++ tests/BlinkTest.php
@@ -0,0 +1,331 @@
+<?php
+
+namespace Spatie\Blink\Test;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\Blink\Blink;
+
+class BlinkTestTest extends TestCase
+{
+    /** @var string */
+    protected $storageFile;
+
+    /** @var \Spatie\Blink\Blink */
+    protected $blink;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->blink = new Blink();
+    }
+
+    /** @test */
+    public function it_can_store_a_key_value_pair()
+    {
+        $this->blink->put('key', 'value');
+
+        $this->assertSame('value', $this->blink->get('key'));
+    }
+
+    /** @test */
+    public function it_can_store_an_array()
+    {
+        $testArray = ['one' => 1, 'two' => 2];
+
+        $this->blink->put('key', $testArray);
+
+        $this->assertSame($testArray, $this->blink->get('key'));
+    }
+
+
+
+    /** @test */
+    public function it_can_determine_if_the_blink_cache_holds_a_value_for_a_given_name()
+    {
+        $this->assertFalse($this->blink->has('key'));
+
+        $this->blink->put('key', 'value');
+
+        $this->assertTrue($this->blink->has('key'));
+    }
+
+    /** @test */
+    public function it_will_return_the_default_value_when_using_a_non_existing_key()
+    {
+        $this->assertSame('default', $this->blink->get('key', 'default'));
+    }
+
+    /** @test */
+    public function it_can_store_an_integer()
+    {
+        $this->blink->put('number', 1);
+
+        $this->assertSame(1, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_provides_a_chainable_put_method()
+    {
+        $this->blink
+            ->put('number', 1)
+            ->put('string', 'hello');
+
+        $this->assertSame(1, $this->blink->get('number'));
+        $this->assertSame('hello', $this->blink->get('string'));
+    }
+
+    /** @test */
+    public function it_will_return_null_for_a_non_existing_value()
+    {
+        $this->assertNull($this->blink->get('non existing key'));
+    }
+
+    /** @test */
+    public function it_can_overwrite_a_value()
+    {
+        $this->blink->put('key', 'value');
+
+        $this->blink->put('key', 'otherValue');
+
+        $this->assertSame('otherValue', $this->blink->get('key'));
+    }
+
+    /** @test */
+    public function it_can_fetch_all_values_at_once()
+    {
+        $this->blink->put('key', 'value');
+
+        $this->blink->put('otherKey', 'otherValue');
+
+        $this->assertSame([
+            'key' => 'value',
+            'otherKey' => 'otherValue',
+        ], $this->blink->all());
+    }
+
+    /** @test */
+    public function it_can_store_multiple_value_pairs_in_one_go()
+    {
+        $values = [
+            'key' => 'value',
+            'otherKey' => 'otherValue',
+        ];
+
+        $this->blink->put($values);
+
+        $this->assertSame('value', $this->blink->get('key'));
+
+        $this->assertSame($values, $this->blink->all());
+    }
+
+    /** @test */
+    public function it_can_store_values_without_forgetting_the_old_values()
+    {
+        $this->blink->put('test1', 'value1');
+        $this->blink->put('test2', 'value2');
+
+        $this->assertSame([
+            'test1' => 'value1',
+            'test2' => 'value2',
+        ], $this->blink->all());
+
+        $this->blink->put(['test3' => 'value3']);
+
+        $this->assertSame([
+            'test1' => 'value1',
+            'test2' => 'value2',
+            'test3' => 'value3',
+        ], $this->blink->all());
+    }
+
+    /** @test */
+    public function it_can_fetch_all_values_starting_with_a_certain_value()
+    {
+        $this->blink->put([
+            'group1Key1' => 'valueGroup1Key1',
+            'group1Key2' => 'valueGroup1Key2',
+            'testgroup1' => 'valueTestGroup1',
+            'group2Key1' => 'valueGroup2Key1',
+            'group2Key2' => 'valueGroup2Key2',
+        ]);
+
+        $expectedArray = [
+            'group1Key1' => 'valueGroup1Key1',
+            'group1Key2' => 'valueGroup1Key2',
+        ];
+
+        $this->assertSame($expectedArray, $this->blink->allStartingWith('group1'));
+    }
+
+    /** @test */
+    public function it_can_forget_a_value()
+    {
+        $this->blink->put('key', 'value');
+        $this->blink->put('otherKey', 'otherValue');
+        $this->blink->put('otherKey2', 'otherValue2');
+
+        $this->blink->forget('otherKey');
+
+        $this->assertSame('value', $this->blink->get('key'));
+
+        $this->assertNull($this->blink->get('otherKey'));
+
+        $this->assertSame('otherValue2', $this->blink->get('otherKey2'));
+    }
+
+    /** @test */
+    public function it_can_flush_the_entire_value_store()
+    {
+        $this->blink->put('key', 'value');
+
+        $this->blink->put('otherKey', 'otherValue');
+
+        $this->blink->flush();
+
+        $this->assertNull($this->blink->get('key'));
+
+        $this->assertNull($this->blink->get('otherKey'));
+    }
+
+    /** @test */
+    public function it_can_flush_all_keys_starting_with_a_certain_string()
+    {
+        $this->blink->put([
+            'group1' => 'valueGroup1',
+            'group1Key1' => 'valueGroup1Key1',
+            'group1Key2' => 'valueGroup1Key2',
+            'group2Key1' => 'valueGroup2Key1',
+            'group2Key2' => 'valueGroup2Key2',
+        ]);
+
+        $this->blink->flushStartingWith('group1');
+
+        $expectedArray = [
+            'group2Key1' => 'valueGroup2Key1',
+            'group2Key2' => 'valueGroup2Key2',
+        ];
+
+        $this->assertSame($expectedArray, $this->blink->all());
+    }
+
+    /** @test */
+    public function it_will_return_an_empty_array_when_getting_all_content()
+    {
+        $this->assertSame([], $this->blink->all());
+    }
+
+    /** @test */
+    public function it_can_get_and_forget_a_value()
+    {
+        $this->blink->put('key', 'value');
+
+        $this->assertSame('value', $this->blink->pull('key'));
+
+        $this->assertNull($this->blink->get('key'));
+    }
+
+    /** @test */
+    public function it_can_increment_a_new_value()
+    {
+        $returnValue = $this->blink->increment('number');
+
+        $this->assertSame(1, $returnValue);
+
+        $this->assertSame(1, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_can_increment_an_existing_value()
+    {
+        $this->blink->put('number', 1);
+
+        $returnValue = $this->blink->increment('number');
+
+        $this->assertSame(2, $returnValue);
+
+        $this->assertSame(2, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_can_increment_a_value_by_another_value()
+    {
+        $returnValue = $this->blink->increment('number', 2);
+
+        $this->assertSame(2, $returnValue);
+
+        $this->assertSame(2, $this->blink->get('number'));
+
+        $returnValue = $this->blink->increment('number', 2);
+
+        $this->assertSame(4, $returnValue);
+
+        $this->assertSame(4, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_can_decrement_a_new_value()
+    {
+        $returnValue = $this->blink->decrement('number');
+
+        $this->assertSame(-1, $returnValue);
+
+        $this->assertSame(-1, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_can_decrement_an_existing_value()
+    {
+        $this->blink->put('number', 10);
+
+        $returnValue = $this->blink->decrement('number');
+
+        $this->assertSame(9, $returnValue);
+
+        $this->assertSame(9, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_can_decrement_a_value_by_another_value()
+    {
+        $returnValue = $this->blink->decrement('number', 2);
+
+        $this->assertSame(-2, $returnValue);
+
+        $this->assertSame(-2, $this->blink->get('number'));
+
+        $returnValue = $this->blink->decrement('number', 2);
+
+        $this->assertSame(-4, $returnValue);
+
+        $this->assertSame(-4, $this->blink->get('number'));
+    }
+
+    /** @test */
+    public function it_implements_array_access()
+    {
+        $this->assertFalse(isset($this->blink['key']));
+
+        $this->blink['key'] = 'value';
+
+        $this->assertTrue(isset($this->blink['key']));
+
+        $this->assertSame('value', $this->blink['key']);
+
+        unset($this->blink['key']);
+
+        $this->assertFalse(isset($this->blink['key']));
+
+        $this->assertNull($this->blink['key']);
+    }
+
+    /** @test */
+    public function it_implements_countable()
+    {
+        $this->assertCount(0, $this->blink);
+
+        $this->blink->put('key', 'value');
+
+        $this->assertCount(1, $this->blink);
+    }
+}
